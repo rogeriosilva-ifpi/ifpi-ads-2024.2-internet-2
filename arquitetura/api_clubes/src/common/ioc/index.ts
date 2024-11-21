8222404ebc9ad31cc95f6fc3fbfcd553a5d1b7eb
Rogério Silva <rogerio410@gmail.com>
2024-11-21 18:30:03 -0300

    DIP, IoC and friends++

--- arquitetura/api_clubes/src/common/ioc/index.ts
+++ arquitetura/api_clubes/src/common/ioc/index.ts
@@ -1,5 +1,28 @@
 import { container } from "tsyringe";
 import { MatchRepository } from "../../domain/repositories/match.repository";
 import { DrizzleMatchRepository } from "../../persistence/drizzle/drizzle.match.repository";
+import { TypeORMMatchRepository } from "../../persistence/typeorm/typeorm.match.repository";
 
-container.register<MatchRepository>("MatchRepository", DrizzleMatchRepository);
+// ******* INJEÇÃO DIRETA *******
+
+// container.register<MatchRepository>("MatchRepository", TypeORMMatchRepository);
+// container.register<MatchRepository>("MatchRepository", DrizzleMatchRepository);
+
+// ******* INJEÇÃO COM REGRAS *******
+
+// Usar TypeORM pela manhã (0-11h) e Drizzle à tarde/noite (12-23h)
+const matchRepositoryFactory = (): MatchRepository => {
+  const currentHour = new Date().getHours();
+
+  // Experimento trocar
+  if (currentHour < 12) {
+    return new TypeORMMatchRepository();
+  } else {
+    return new DrizzleMatchRepository();
+  }
+};
+
+// Registro da Fábrica no container
+container.register<MatchRepository>("MatchRepository", {
+  useFactory: matchRepositoryFactory,
+});
